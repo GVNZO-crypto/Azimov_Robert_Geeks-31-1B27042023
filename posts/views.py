32908bf35ce59ce1f1d42961dba4bfb8b5e05365
GVNZO <azimovrobert8@gmail.com>
2023-08-24 06:53:55 +0600

    first h/w

---
+++ posts/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import HttpResponse
+from datetime import datetime
+# Create your views here.
+
+def main_view(request):
+    if request.method == 'GET':
+        return HttpResponse("This is my first project")
+        
+def hello_views(request):
+    return HttpResponse("Hello it's my new project")
+
+def time(request):
+    date = datetime.now().strftime("%d-%m-%Y - %H:%M:%S")
+    return HttpResponse (date)
+
+def goodbye(request):
+    return HttpResponse("Goodbye user!")
