08ac4c725ceb76ee5e40a1806a93aa8ae23e27a0
GVNZO <134852934+GVNZO-crypto@users.noreply.github.com>
2023-08-28 22:53:32 +0600

    fifth h\w

--- Product/views.py
+++ Product/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponseRedirect
+
 from Product.models import Product, Category
 from Product.forms import ReviewForm, ProductCreateForm, CategoryCreateForm
 
 # Основной вид
 def main_view(request):
     return render(request, 'layouts/index.html')
+
 
 # Вид продукта
 def product_view(request):
@@ -16,6 +18,7 @@ def product_view(request):
         }
         return render(request, 'products/products.html', context=context_data)
 
+
 # Вид категории
 def category(request):
     categories = Category.objects.all()
@@ -23,6 +26,7 @@ def category(request):
         "categories": categories
     }
     return render(request, 'categories.html', context=context_data)
+
 
 # Подробное представление продукта
 def product_detail_view(request, id):
@@ -42,7 +46,9 @@ def product_detail_view(request, id):
     }
     return render(request, 'products/detail.html', context=context_data)
 
+
 # БЛОК ПОЛЬЗОВАТЕЛЬСКИХ СТРАНИЦ #
+
 # Создание продукта пользователем
 def create_product(request):
     if request.method == 'POST':
@@ -54,14 +60,16 @@ def create_product(request):
         product_form = ProductCreateForm()
     return render(request, 'products/create_product.html', {'product_form': product_form})
 
+
 # Создание категории пользователем
 def create_category(request):
     if request.method == 'POST':
         category_form = CategoryCreateForm(request.POST, request.FILES)
         if category_form.is_valid():
             category_form.save()
-            return redirect('category') 
+            return redirect('category')
     else:
         category_form = CategoryCreateForm()
     return render(request, 'products/create_categories.html', {'category_form': category_form})
+
 # БЛОК ПОЛЬЗОВАТЕЛЬСКИХ СТРАНИЦ #
